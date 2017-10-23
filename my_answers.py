a164bd8472990cd9efbf5189f8733274712e0cb0
Va1da2 <vaidas.armonas@gmail.com>
2017-10-23 06:20:14 +0300

    first commit

---
+++ my_answers.py
@@ -0,0 +1,64 @@
+import numpy as np
+import re
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+import keras
+
+
+# TODO: fill out the function below that transforms the input series 
+# and window-size into a set of input/output pairs for use with our RNN model
+def window_transform_series(series, window_size):
+    # containers for input/output pairs
+    X = []
+    y = []
+
+    max_index = len(series) - window_size
+    idx = 0
+    for i in range(0, max_index, 1):
+        idx_end = i + window_size
+        X.append(series[i: idx_end])
+        y.append(series[idx_end:idx_end+1])
+    # reshape each 
+    X = np.asarray(X)
+    X.shape = (np.shape(X)[0:2])
+    y = np.asarray(y)
+    y.shape = (len(y),1)
+
+    return X,y
+
+# TODO: build an RNN to perform regression on our time series input/output data
+def build_part1_RNN(window_size):
+    model = Sequential()
+    model.add(LSTM(5, input_shape=(window_size, 1)))
+    model.add(Dense(1))
+
+    return model
+
+
+### TODO: return the text input with only ascii lowercase and the punctuation given below included.
+def cleaned_text(text):
+    punctuation = ['!', ',', '.', ':', ';', '?']
+    pattern = '[^0-9a-zA-Z' + ''.join(punctuation) + ']'
+    text2 = re.sub(pattern, ' ', text)
+    return text2
+
+### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
+def window_transform_text(text, window_size, step_size):
+    # containers for input/output pairs
+    inputs = []
+    outputs = []
+    max_idx = len(text) - window_size
+    for i in range(0, max_idx, step_size):
+        inputs.append(text[i : i + window_size])
+        outputs.append(text[i + window_size])
+    return inputs,outputs
+
+# TODO build the required RNN model: 
+# a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
+def build_part2_RNN(window_size, num_chars):
+    model = Sequential()
+    model.add(LSTM(200, input_shape=(window_size, num_chars)))
+    model.add(Dense(num_chars, activation='softmax'))
+    return model
